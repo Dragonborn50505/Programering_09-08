0608ad73367ee3a42147cf3c5bd743cc8875c243
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2021-09-14 10:46:03 +0200

    Start

---
+++ Programering_09-08/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.WriteLine("Då är blöt och har gått i timmar du kommer fram till en vägkorsning. Du kan antingn gå höger eller vänster, vilken väg välger du?");
+string choice = Console.ReadLine(); 
+
+choice = choice.ToLower(); 
+
+
+if (choice == "höger")
+{
+Console.WriteLine("Efter ha föjlt den högra texten så hittar du en grotta. Medans du utforskar den så hittar du en sovande drake. Försöker du döda den eller springa");
+
+}
