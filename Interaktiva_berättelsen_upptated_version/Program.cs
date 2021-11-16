2928d1a3971721f98e4e27387b2f4f58bfd3b42c
Vincent Allinggård <allinggrd.vincent@elev.ga.ntig.se>
2021-11-16 11:04:51 +0100

    save

--- Interaktiva_berättelsen_upptated_version/Program.cs
+++ Interaktiva_berättelsen_upptated_version/Program.cs
@@ -5,36 +5,21 @@ string currentRoom = "crossroads";
 string option = "";
 
 while (currentRoom != "gameover")
+currentRoom == Crossroads();
 {
     if (currentRoom == "crossroads")
     {
-        Console.WriteLine("Då är blöt och har gått i timmar du kommer fram till en vägkorsning. Du kan antingn gå höger eller vänster, vilken väg välger du?");
-        option = Console.ReadLine();
-        option = option.ToLower();
-      
-        if (option == "vänster")
-        {
-            currentRoom = "house";
-        }
-
-        else if (option == "höger")
-        {
-            currentRoom = "cave";
-        }
-
-
-
-
-
+        Console.WriteLine("Förklaring");
+        
     }
     else if (currentRoom == "house")
     {
-if (option == "knock")
+        if (option == "knock")
         {
             currentRoom = "knock";
         }
 
-        
+
     }
 
 
@@ -49,20 +34,37 @@ if (option == "knock")
 
     }
 
-     else if (currentRoom == "fortästt")
+    else if (currentRoom == "fortästt")
     {
 
     }
 
-     else if (currentRoom == "Kill")
+    else if (currentRoom == "Kill")
     {
 
     }
 
 
-    
+
 }
 
+
+static string Crossroads()
+{
+    Console.WriteLine("Då är blöt och har gått i timmar du kommer fram till en vägkorsning. Du kan antingn gå höger eller vänster, vilken väg välger du?");
+    string option = Console.ReadLine();
+    option = option.ToLower();
+
+    if (option == "vänster")
+    {
+        return "house";
+    }
+
+    else if (option == "höger")
+    {
+        return "cave";
+    }
+}
 
 
 
@@ -79,7 +81,7 @@ if (choice == "höger") // Första möjliga vallet med flera val på väg
 
     if (choice == "döda" || choice == "kill" || choice == "död")
     {
-        Console.ForegroundColor = ConsoleColor.Red; 
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Du dog... Det är en drake vad trode du? GAME OVER");
         choice = Console.ReadLine();
     }
@@ -96,18 +98,21 @@ if (choice == "höger") // Första möjliga vallet med flera val på väg
             choice = Console.ReadLine();
 
             if (choice == "knacka")
-            {Console.ForegroundColor = ConsoleColor.Green; 
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Du knackade på men ingen svara... Detta är för detta är en demo och du kommer inget längre. Slut");
                 choice = Console.ReadLine();
             }
             if (choice == "gå" || choice == "fortsätt")
-            {Console.ForegroundColor = ConsoleColor.Blue; 
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("Du fortsatte gå men du hittade inga mer hus för tillverkarna orkade inte lägga in fler. Slut");
                 choice = Console.ReadLine();
             }
         }
         if (choice == "höger")
-        {Console.ForegroundColor = ConsoleColor.Magenta; 
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("Varför?? Du har redan gått den här vägen?? Vi trode ingen skulle gå höger igen så det här är allt ni får! SLUT!!");
             choice = Console.ReadLine();    // Undrar hur många försöker få det här slutet
         }
@@ -121,14 +126,16 @@ if (choice == "vänster")
 {
     Console.WriteLine("Du gick den vänstra vägen och efter ett tag kom fram till ett hus, knackar du på eller fortsätter du");
     choice = Console.ReadLine();
-    
+
     if (choice == "knacka")
-    {Console.ForegroundColor = ConsoleColor.Green; 
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Du knackade på men ingen svara... Detta är för detta är en demo och du kommer inget längre. Slut");
         choice = Console.ReadLine(); //Orkade inte fortsätta på det här spelet, kommer förmodligen aldrig bli klar men det säger vi inte till dom
     }
     if (choice == "gå" || choice == "fortsätt")
-    {Console.ForegroundColor = ConsoleColor.Blue; 
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("Du fortsatte gå men du hittade inga mer hus för tillverkarna orkade inte lägga in fler. Slut");
         choice = Console.ReadLine();//Joan du vet att jag läser igenom alla av dina koder, gå tillbaka till arbete annars måste jag säga till bossen
     }
@@ -136,3 +143,6 @@ if (choice == "vänster")
 }
 
 Console.ReadLine();
+
+
+//while
